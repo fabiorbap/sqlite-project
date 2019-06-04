e23c8cc14e52de98c30641d139c6053630bea3b6
Fabio Rachid <fabiorbap@gmail.com>
2019-06-04 18:04:57 -0300

    Created insert and get methods

--- app/src/main/java/br/com/frachid/databasedemo/MainActivity.java
+++ app/src/main/java/br/com/frachid/databasedemo/MainActivity.java
@@ -3,17 +3,41 @@ package br.com.frachid.databasedemo;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseAdapter databaseAdapter;
+    EditText name, email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
         setupDatabaseHelper();
     }
 
     private void setupDatabaseHelper() {
+        databaseAdapter = new DatabaseAdapter(this);
+    }
 
+    public void addUser(View view) {
+        String nameValue = name.getText().toString().trim();
+        String emailValue = email.getText().toString().trim();
+        long id = databaseAdapter.insertData(nameValue, emailValue);
+        if (id < 0){
+            Toast.makeText(this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "One record successfully inserted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void viewUsers(View view) {
+        Toast.makeText(this, databaseAdapter.getAllData(), Toast.LENGTH_LONG).show();
     }
 }
